9fe64d7d31aac42d9db565da6cf149bd22e8277f
e145747 <e145747@ie.u-ryukyu.ac.jp>
2015-12-17 10:16:35 +0900

    テキストウィンドウ(最低限機能)の追加

---
+++ Assets/Scripts/textest.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class textest : MonoBehaviour {
+
+	public Text score;
+
+	void Start () {	
+	}
+
+	void Update () {
+		score.text = "てすと\n表示どうですか？";
+	}
+}
