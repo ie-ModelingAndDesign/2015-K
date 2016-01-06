a99c555b6a87df3b3c1298e424a34e3f145061b1
e145747 <e145747@ie.u-ryukyu.ac.jp>
2016-01-06 18:40:34 +0900

    テキスト表示中に移動を制限する機能の実装

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,71 +4,77 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class PlayerMove : MonoBehaviour
 {
-	// 移動速度
 	public float speed = 3;
 
 	void Update()
 	{
-		// 移動方向(左右)
 		float x = CrossPlatformInputManager.GetAxisRaw ("Horizontal");
-		// 移動方向(上下)
 		float y = CrossPlatformInputManager.GetAxisRaw ("Vertical");
 
-		if (-0.3 <= x && x <= 0.3)
+		if (FlagManager.Instance.flags[101] == false)
 		{
-			if (0.3 <= y)
+			if (-0.3 <= x && x <= 0.3)
 			{
-				Vector2 direction = new Vector2 (0, y).normalized;
-				GetComponent<Rigidbody2D>().velocity = direction * speed;
-				FlagManager.Instance.flags[1] = true;
-				FlagManager.Instance.flags[2] = false;
-				FlagManager.Instance.flags[3] = false;
-				FlagManager.Instance.flags[4] = false;
-				Debug.Log ("N");
+				if (0.3 <= y)
+				{
+					Vector2 direction = new Vector2 (0, y).normalized;
+					GetComponent<Rigidbody2D>().velocity = direction * speed;
+					FlagManager.Instance.flags[1] = true;
+					FlagManager.Instance.flags[2] = false;
+					FlagManager.Instance.flags[3] = false;
+					FlagManager.Instance.flags[4] = false;
+					Debug.Log ("N");
+				}
+
+				else if (y <= -0.3)
+				{
+					Vector2 direction = new Vector2 (0, y).normalized;
+					GetComponent<Rigidbody2D>().velocity = direction * speed;
+					FlagManager.Instance.flags[1] = false;
+					FlagManager.Instance.flags[2] = true;
+					FlagManager.Instance.flags[3] = false;
+					FlagManager.Instance.flags[4] = false;
+					Debug.Log ("S");
+				}
+
+				else
+				{
+					Vector2 direction = new Vector2 (0, 0).normalized;
+					GetComponent<Rigidbody2D>().velocity = direction * speed;
+				}
 			}
 
-			else if (y <= -0.3)
+			else if (-0.3 <= y && y <= 0.3)
 			{
-				Vector2 direction = new Vector2 (0, y).normalized;
-				GetComponent<Rigidbody2D>().velocity = direction * speed;
-				FlagManager.Instance.flags[1] = false;
-				FlagManager.Instance.flags[2] = true;
-				FlagManager.Instance.flags[3] = false;
-				FlagManager.Instance.flags[4] = false;
-				Debug.Log ("S");
+				if (x <= -0.3)
+				{
+					Vector2 direction = new Vector2 (x, 0).normalized;
+					GetComponent<Rigidbody2D>().velocity = direction * speed;
+					FlagManager.Instance.flags[1] = false;
+					FlagManager.Instance.flags[2] = false;
+					FlagManager.Instance.flags[3] = true;
+					FlagManager.Instance.flags[4] = false;
+					Debug.Log ("W");
+				}
+
+				else if (0.3 <= x)
+				{
+					Vector2 direction = new Vector2 (x, 0).normalized;
+					GetComponent<Rigidbody2D>().velocity = direction * speed;
+					FlagManager.Instance.flags[1] = false;
+					FlagManager.Instance.flags[2] = false;
+					FlagManager.Instance.flags[3] = false;
+					FlagManager.Instance.flags[4] = true;
+					Debug.Log ("E");
+				}
+
+				else
+				{
+					Vector2 direction = new Vector2 (0, 0).normalized;
+					GetComponent<Rigidbody2D>().velocity = direction * speed;
+				}
 			}
 
-			else
-			{
-				Vector2 direction = new Vector2 (0, 0).normalized;
-				GetComponent<Rigidbody2D>().velocity = direction * speed;
-			}
-		}
-
-		else if (-0.3 <= y && y <= 0.3)
-		{
-			if (x <= -0.3)
-			{
-				Vector2 direction = new Vector2 (x, 0).normalized;
-				GetComponent<Rigidbody2D>().velocity = direction * speed;
-				FlagManager.Instance.flags[1] = false;
-				FlagManager.Instance.flags[2] = false;
-				FlagManager.Instance.flags[3] = true;
-				FlagManager.Instance.flags[4] = false;
-				Debug.Log ("W");
-			}
-			
-			else if (0.3 <= x)
-			{
-				Vector2 direction = new Vector2 (x, 0).normalized;
-				GetComponent<Rigidbody2D>().velocity = direction * speed;
-				FlagManager.Instance.flags[1] = false;
-				FlagManager.Instance.flags[2] = false;
-				FlagManager.Instance.flags[3] = false;
-				FlagManager.Instance.flags[4] = true;
-				Debug.Log ("E");
-			}
-			
 			else
 			{
 				Vector2 direction = new Vector2 (0, 0).normalized;
