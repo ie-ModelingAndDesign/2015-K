a8c283b54b21fa797d86020d545477fb390000a7
e145717 <e145717@ie.u-ryukyu.ac.jp>
2016-01-09 00:37:54 +0900

    地下（仮）

---
+++ Assets/Scripts/UG_Scripts/UG_wall_goal_text.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UG_wall_goal_text: MonoBehaviour {
+
+
+	public Text score;
+
+	// Use this for initialization
+	void Start () {
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (FlagManager.Instance.flags [22] == true) {
+
+
+			FlagManager.Instance.flags [101] = true;
+			score.text = "おや？\n壁になにかかかっているようだ\n部屋にもってかえろう";
+
+
+			if (FlagManager.Instance.flags [0] == true) {
+
+				score.text = "";
+
+				FlagManager.Instance.flags [0] = false;
+				FlagManager.Instance.flags [22] = false;
+				FlagManager.Instance.flags [101] = false;
+				Application.LoadLevel ("Stage");
+
+
+			}
+		}
+	
+	}
+}
