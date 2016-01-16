56f0440b84bda2942b06244aecc1a363ae664f3d
e145747 <e145747@ie.u-ryukyu.ac.jp>
2016-01-16 23:44:16 +0900

    ウィンドウメッセージ追加

---
+++ Assets/Scripts/ObjectText/Text_DustBox.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Text_DustBox : MonoBehaviour {
+
+	public Text score;
+	
+	void Start () {
+		
+		FlagManager.Instance.flags [101] = false;
+		FlagManager.Instance.flags [121] = false;
+		FlagManager.Instance.flags [122] = false;
+		FlagManager.Instance.flags [123] = false;
+		FlagManager.Instance.flags [124] = false;
+		FlagManager.Instance.flags [125] = false;
+		FlagManager.Instance.flags [126] = false;
+		FlagManager.Instance.flags [127] = false;
+	}
+	
+	// 22文字まで(全角・句読点含む)
+	
+	void Update () {
+		if (FlagManager.Instance.flags[7] == false && FlagManager.Instance.flags [17] == true)
+		{
+			score.text = "ゴミ箱だ。\nゴミであふれていっぱいになっている。";
+			
+			if (FlagManager.Instance.flags [121] == false)
+			{
+				WaitTime();
+				
+				if (FlagManager.Instance.flags [0] == true)
+				{
+					FlagManager.Instance.flags [121] = true;
+					FlagManager.Instance.flags [101] = false;
+				}
+			}
+			
+			if (FlagManager.Instance.flags [121] == true)
+			{
+                score.text = "";
+
+                WaitTime();
+                FlagManager.Instance.flags [8] = false;
+                FlagManager.Instance.flags [17] = false;
+			}
+		}
+	}
+	
+	void WaitTime()
+	{
+		if (FlagManager.Instance.flags [101] == false)
+		{
+			FlagManager.Instance.flags [0] = false;
+			FlagManager.Instance.flags [101] = true;
+		}
+	}
+}
