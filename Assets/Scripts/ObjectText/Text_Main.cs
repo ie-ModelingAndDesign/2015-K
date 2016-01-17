f5dec18c4f2f1e9b6814bd6767eea7a485ad2934
e145747 <e145747@ie.u-ryukyu.ac.jp>
2016-01-17 12:46:27 +0900

    一部キャンバスサイズの変更 / シナリオ進行

---
+++ Assets/Scripts/ObjectText/Text_Main.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Text_Main : MonoBehaviour {
+
+	public Text score;
+	
+	void Start () {
+		FlagManager.Instance.flags [101] = false;
+		FlagManager.Instance.flags [121] = false;
+		FlagManager.Instance.flags [122] = false;
+		FlagManager.Instance.flags [123] = false;
+		FlagManager.Instance.flags [124] = false;
+		FlagManager.Instance.flags [125] = false;
+		FlagManager.Instance.flags [126] = false;
+		FlagManager.Instance.flags [127] = false;
+	}
+	
+	// 22文字まで(全角・句読点含む)
+	
+	void Update () {
+		if (FlagManager.Instance.flags[7] == false && FlagManager.Instance.flags [18] == true)
+		{
+			GameObject gameObject = this.transform.Find ("Main").gameObject;
+			gameObject.SetActive(true);
+
+			score.text = "とても大きなタンスだ。\n全部で9個の引き出しが付いている。";
+			
+			if (FlagManager.Instance.flags [121] == false)
+			{
+				WaitTime();
+				
+				if (FlagManager.Instance.flags [0] == true)
+				{
+					FlagManager.Instance.flags [121] = true;
+					FlagManager.Instance.flags [101] = false;
+				}
+			}
+			
+			if (FlagManager.Instance.flags [121] == true)
+			{
+				score.text = "上下左右4つの引き出しには、\nそれぞれ不思議な記号が描かれている。";
+				
+				if (FlagManager.Instance.flags [122] == false)
+				{
+					WaitTime();
+					
+					if (FlagManager.Instance.flags [0] == true)
+					{
+						FlagManager.Instance.flags [122] = true;
+						FlagManager.Instance.flags [101] = false;
+					}
+				}
+				
+				if (FlagManager.Instance.flags [122] == true)
+				{
+					score.text = "";
+					
+					WaitTime();
+					FlagManager.Instance.flags [8] = false;
+					FlagManager.Instance.flags [18] = false;
+
+					gameObject.SetActive(false);
+				}
+			}
+		}
+		
+		else if (FlagManager.Instance.flags [10] == false && FlagManager.Instance.flags [11] == false && FlagManager.Instance.flags [12] == false && FlagManager.Instance.flags [13] == false && FlagManager.Instance.flags [14] == false && FlagManager.Instance.flags [15] == false && FlagManager.Instance.flags [16] == false && FlagManager.Instance.flags [17] == false && FlagManager.Instance.flags [18] == false && FlagManager.Instance.flags [19] == false)
+		{
+			FlagManager.Instance.flags [101] = false;
+			FlagManager.Instance.flags [121] = false;
+			FlagManager.Instance.flags [122] = false;
+			FlagManager.Instance.flags [123] = false;
+			FlagManager.Instance.flags [124] = false;
+			FlagManager.Instance.flags [125] = false;
+			FlagManager.Instance.flags [126] = false;
+			FlagManager.Instance.flags [127] = false;
+		}
+	}
+	
+	void WaitTime()
+	{
+		if (FlagManager.Instance.flags [101] == false)
+		{
+			FlagManager.Instance.flags [0] = false;
+			FlagManager.Instance.flags [101] = true;
+		}
+	}
+}
---
+++ Assets/Scripts/Others/InactivePic.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InactivePic : MonoBehaviour {
+
+	void Start () {
+		
+		gameObject.SetActive(false);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
