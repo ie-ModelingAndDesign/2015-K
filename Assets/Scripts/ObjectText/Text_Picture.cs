ef063bb3757ddf52cf92e7f09ac28e4a3adc45cf
e145747 <e145747@ie.u-ryukyu.ac.jp>
2016-02-09 15:30:40 +0900

    シナリオ進行 - 3つ目の謎解き(途中) / 新規画像の追加(間違い探し用)

--- Assets/Scripts/ObjectText/Text_Picture.cs
+++ Assets/Scripts/ObjectText/Text_Picture.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 public class Text_Picture : MonoBehaviour {
 
 	public Text score;
+	int i;
 	
 	void Start () {
 		
@@ -21,7 +22,7 @@ public class Text_Picture : MonoBehaviour {
 	// 22文字まで(全角・句読点含む)
 	
 	void Update () {
-		if (FlagManager.Instance.flags[7] == false && FlagManager.Instance.flags [15] == true)
+		if (FlagManager.Instance.flags[7] == false && FlagManager.Instance.flags [15] == true && FlagManager.Instance.flags [88] == false && FlagManager.Instance.flags [89] == false)
 		{
 			score.text = "壁に絵がかかっている。";
 			
@@ -61,8 +62,92 @@ public class Text_Picture : MonoBehaviour {
 				}
 			}
 		}
+
+		if (FlagManager.Instance.flags[7] == false && FlagManager.Instance.flags [15] == true && FlagManager.Instance.flags [88] == true && FlagManager.Instance.flags [89] == false && i < 6)
+		{
+			score.text = "壁に絵がかかっている。調べてみよう。";
+			
+			if (FlagManager.Instance.flags [121] == false)
+			{
+				WaitTime();
+				
+				if (FlagManager.Instance.flags [0] == true)
+				{
+					FlagManager.Instance.flags [121] = true;
+					FlagManager.Instance.flags [101] = false;
+				}
+			}
+			
+			if (FlagManager.Instance.flags [121] == true)
+			{
+				score.text = "………………。\n絵のまわりを調べたけど、何も見当たらない。\nもっとよく探してみようかな？";
+				
+				if (FlagManager.Instance.flags [122] == false)
+				{
+					WaitTime();
+					
+					if (FlagManager.Instance.flags [0] == true)
+					{
+						FlagManager.Instance.flags [122] = true;
+						FlagManager.Instance.flags [101] = false;
+					}
+				}
+				
+				if (FlagManager.Instance.flags [122] == true)
+				{
+					score.text = "";
+					
+					WaitTime();
+					FlagManager.Instance.flags [8] = false;
+					FlagManager.Instance.flags [15] = false;
+					i = i + 1;
+				}
+			}
+		}
+
+		if (FlagManager.Instance.flags[7] == false && FlagManager.Instance.flags [15] == true && FlagManager.Instance.flags [88] == true && FlagManager.Instance.flags [89] == false && i == 6)
+		{
+			score.text = "壁に絵がかかっている。調べてみよう。";
+			
+			if (FlagManager.Instance.flags [121] == false)
+			{
+				WaitTime();
+				
+				if (FlagManager.Instance.flags [0] == true)
+				{
+					FlagManager.Instance.flags [121] = true;
+					FlagManager.Instance.flags [101] = false;
+				}
+			}
+			
+			if (FlagManager.Instance.flags [121] == true)
+			{
+				score.text = "………………。\n調べている途中で、絵を落としてしまった！\n危ないから長机に運んでおこう。";
+				
+				if (FlagManager.Instance.flags [122] == false)
+				{
+					WaitTime();
+					
+					if (FlagManager.Instance.flags [0] == true)
+					{
+						FlagManager.Instance.flags [122] = true;
+						FlagManager.Instance.flags [101] = false;
+					}
+				}
+				
+				if (FlagManager.Instance.flags [122] == true)
+				{
+					score.text = "";
+					
+					WaitTime();
+					FlagManager.Instance.flags [8] = false;
+					FlagManager.Instance.flags [15] = false;
+					FlagManager.Instance.flags [89] = true;
+				}
+			}
+		}
 	}
-	
+
 	void WaitTime()
 	{
 		if (FlagManager.Instance.flags [101] == false)
---
+++ Assets/Scripts/Others/InActivePicPic2.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InActivePicPic2 : MonoBehaviour {
+	
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (FlagManager.Instance.flags [89] == true)
+		{
+			GameObject gameObjectPic = this.transform.Find ("Picture2").gameObject;
+			gameObjectPic.SetActive(true);
+		}
+	}
+}
