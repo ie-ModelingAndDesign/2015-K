39e8beb0a400bf0b5f7ccc885f16011ed759d557
e145747 <e145747@ie.u-ryukyu.ac.jp>
2016-02-11 17:57:46 +0900

    軽微な変更

--- Assets/Scripts/ItemSelect/ItemActive.cs
+++ Assets/Scripts/ItemSelect/ItemActive.cs
@@ -46,6 +46,11 @@ public class ItemActive : MonoBehaviour {
 			gameObject1.SetActive(true);
 		}
 
+		if (FlagManager.Instance.flags [67] == true) {
+			GameObject gameObject1 = this.transform.Find ("kami5").gameObject;
+			gameObject1.SetActive(true);
+		}
+
 
 
 		//sonota tuika 61,62,...
