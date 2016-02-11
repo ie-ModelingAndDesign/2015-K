8bd3371a88e9630359430087ba3a3f96bfb26a4f
shingakisho <e14511@ie.u-ryukyu.ac.jp>
2016-02-11 20:47:21 +0900

    commit file 2/11

---
+++ Assets/Scripts/ButtonSelect/op_Button.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections; 
+
+
+public class op_Button: MonoBehaviour {
+
+	public void OnClick() {
+		Debug.Log ("Button click!");
+
+
+		//System.Threading.Thread.Sleep (2000);
+
+		gameObject.SetActive (false);
+		   
+	}
+
+}
---
+++ Assets/Scripts/ButtonSelect/op_Button_tostage.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class op_Button_tostage : MonoBehaviour {
+
+
+	public void OnClick() {
+		Debug.Log("Button click!");
+		Application.LoadLevel ("Stage");
+
+	}
+}
