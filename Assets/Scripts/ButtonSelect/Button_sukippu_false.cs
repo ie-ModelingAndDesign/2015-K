49cde2f0474e3d2fc65e3a7e9cc46fad9779c8fa
shingakisho <e14511@ie.u-ryukyu.ac.jp>
2016-02-15 13:21:23 +0900

    modified ending

---
+++ Assets/Scripts/ButtonSelect/Button_sukippu_false.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections; 
+
+
+public class  Button_sukippu_false: MonoBehaviour {
+	
+	public void OnClick() {
+		Debug.Log ("Button click!");
+
+		gameObject.SetActive (false);
+		
+	}
+	
+}
